ab9dc32f82dfceffaf77bd56229ddc31370bfd6e
joerdixon <joseph35dixon@gmail.com>
2023-01-07 21:35:05 -0800

    added components

---
+++ src/components/Section/Classes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Classes() {
+return(
+<h1>Functional Classes</h1>
+)
+};
+
+export default Classes;
---
+++ src/components/Section/Shows.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Shows() {
+return(
+<h1>Functional Shows</h1>
+)
+};
+
+export default Shows;
