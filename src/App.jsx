5a2259f3bcfe2d1c42553d7acc095574f038be6e
joerdixon <joseph35dixon@gmail.com>
2023-01-07 22:13:43 -0800

    added sass and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import React from "react";
 import Nav from './components/Navigation/Nav.jsx'
 import Hero from './components/Section/Hero.jsx'
 import Bio from './components/Section/Bio.jsx'
+import Classes from './components/Section/Classes.jsx'
+import Shows from './components/Section/Shows.jsx'
+import Contact from './components/Section/Contact.jsx'
 
 export default () => (
   <>
     <Nav />
     <Hero />
     <Bio />
+    <Classes />
+    <Shows />
+    <Contact />
   </>
 );
