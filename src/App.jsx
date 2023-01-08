153d8e8c150d2d7f16d2e602bef4e9ac3aa55d60
joerdixon <joseph35dixon@gmail.com>
2023-01-07 21:01:51 -0800

    Bio skeleton in

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 // Component Imports
 import Nav from './components/Navigation/Nav.jsx'
 import Hero from './components/Section/Hero.jsx'
+import Bio from './components/Section/Bio.jsx'
 
 export default () => (
   <>
     <Nav />
     <Hero />
+    <Bio />
   </>
 );
---
+++ src/components/Section/Bio.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Bio() {
+return(
+<h1>Functional Bio</h1>
+)
+};
+
+export default Bio;
