1d00bd3057d3b78c77dd3a1bd508a2ed766c9b06
joerdixon <joseph35dixon@gmail.com>
2023-01-07 16:31:40 -0800

    deps in

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from "react";
 
 export default () => (
   <>
-    <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
+    <h1>Welcome to Randy Dixon</h1>
   </>
 );
