94fb256e7c2f50f8a322c8eba1f657b94d70ba42
joerdixon <joseph35dixon@gmail.com>
2023-01-07 17:15:38 -0800

    nav skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+// Component Imports
+import Nav from './components/Navigation/Nav.jsx'
 
 export default () => (
   <>
-    <h1>Welcome to Randy Dixon</h1>
+    <h1>Randy Dixon</h1>
+    <Nav />
   </>
 );
---
+++ src/components/Navigation/Nav.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Nav() {
+    return(
+        <h1>Navigation</h1>
+    )
+}
+
+export default Nav;
